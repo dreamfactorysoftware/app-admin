b668265aabca345d60b4cabbb980e274992daf20
Jason Sykes <jason@jasonsykes.com>
2013-01-23 17:36:55 -0500

    New editor, uses ACE, Actions class

--- filemanager/js/editor.js
+++ filemanager/js/editor.js
@@ -1,107 +1,63 @@
 /**
  * editor.js
  */
-document.write('<title>Editing: '+CommonUtilities.getQueryParameter('path')+'</title>');
+//document.write('<title>Editing: '+CommonUtilities.getQueryParameter('path')+'</title>');
 
 
-function setSelectionRange(input, selectionStart, selectionEnd) {
-	if (input.setSelectionRange) {
-		input.focus();
-		input.setSelectionRange(selectionStart, selectionEnd);
-	} else if (input.createTextRange) {
-		var range = input.createTextRange();
-		range.collapse(true);
-		range.moveEnd('character', selectionEnd);
-		range.moveStart('character', selectionStart);
-		range.select();
-	}
-}
+Actions = {
+    getFile : function(){
+        $.ajax({
+            url:'http://' + location.host + '/rest/app/' + Actions.getQueryParameter('path'),
+            data:'app_name=filemanager&method=GET',
+            cache:false,
+            success:function (response) {
+               Actions.loadEditor(response);
+            },
+            error:function (response) {
+                if (response.status == 401) {
+                    $("#loginDialog").modal('toggle');
+                }
+            }
+        });
+    },
+    saveFile:function(){
+        $.ajax({
+            url:'http://' + location.host + '/rest/app/' + Actions.getQueryParameter('path'),
+            data:'app_name=filemanager&method=MERGE',
+            type:'POST',
+            cache:false,
+            success:function (response) {
+                //window.close();
+            },
+            error:function (response) {
+                if (response.status == 401) {
+                    $("#loginDialog").modal('toggle');
+                }
+            }
+        });
+    },
+    loadEditor:function(contents){
+        console.log('loaded');
+        Editor = ace.edit("editor");
+        Editor.setTheme("ace/theme/twilight");
+        Editor.getSession().setMode("ace/mode/javascript");
+        Editor.setValue(contents);
 
-function replaceSelection (input, replaceString) {
-	if (input.setSelectionRange) {
-		var selectionStart = input.selectionStart;
-		var selectionEnd = input.selectionEnd;
-		input.value = input.value.substring(0, selectionStart)+ replaceString + input.value.substring(selectionEnd);
-  
-		if (selectionStart != selectionEnd){ 
-			setSelectionRange(input, selectionStart, selectionStart + 	replaceString.length);
-		} else {
-			setSelectionRange(input, selectionStart + replaceString.length, selectionStart + replaceString.length);
-		}
-	} else if (document.selection) {
-		var range = document.selection.createRange();
-		
-		if (range.parentElement() == input) {
-			var isCollapsed = range.text == '';
-			range.text = replaceString;
+        $("#save").click(function(){
+           Actions.saveFile();
+        });
 
-			if (!isCollapsed)  {
-				range.moveStart('character', -replaceString.length);
-				range.select();
-			}
-		}
-	}
-}
-
-function catchTab(item,e){
-	if(navigator.userAgent.match("Gecko")){
-		c=e.which;
-	} else {
-		c=e.keyCode;
-	}
-	
-	if(c==9){
-		replaceSelection(item,String.fromCharCode(9));
-		return false;
-	}	    
-}
-
-
-var fileio = new DFRequest({
-	app: CommonUtilities.getQueryParameter('hostApp'),
-	service: "APP",
-	dataType: 'text',
-	contentType: 'text/html',
-	resource: "/"+CommonUtilities.getQueryParameter('path'),
-	success: function(txt,request) {
-		try {
-			var json = JSON.parse(txt);
-			var err = checkFailure(json);
-			if(err) {
-				$('#errorMsg').html(err);
-				$('#errorDialog').dialog('open');
-			} else {
-				// TODO: display something to demostrate the save is done...
-			}
-		} catch (e) {
-			$("#editor").val(txt);
-		}
-	}
-});
-
+        $("#close").click(function(){
+            window.close();
+        });
+    },
+    getQueryParameter: function(key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&");
+        var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    }
+};
 $(document).ready(function(){
-	$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
-	fileio.retrieve();
-	
-	$("#errorDialog").dialog({
-		resizable: false,
-		modal: true,
-		autoOpen: false,
-		closeOnEscape: true,
-		buttons: {	}
-	});
-	
-	$("#save").button({icons:{primary:"ui-icon-disk"}}).click(function(){
-		var value = $('#editor').val();
-		var name = fileio.request.resource;
-		name = name.substring(name.lastIndexOf('/')+1);
-		if(name.toLowerCase().indexOf('.html')) {
-			fileio.upload({"name": name,"raw":value},null,null,true);
-		}
-	});
-	
-	$("#close").button({icons:{primary:"ui-icon-close"}}).click(function(){
-		window.close();
-	});
+	Actions.getFile();
 	
 });
