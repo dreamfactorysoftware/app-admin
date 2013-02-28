b082c4edb581d3d53262e4831d3e75149244ee76
Jason Sykes <jason@jasonsykes.com>
2013-02-27 20:27:51 -0500

    Role.Service.Accesses  Initial Commit

--- js/controllers/rolectrl.js
+++ js/controllers/rolectrl.js
@@ -1,12 +1,29 @@
-var RoleCtrl = function ($scope, RolesRelated, User, App) {
+var RoleCtrl = function ($scope, RolesRelated, User, App, Service, $http) {
     Scope = $scope;
-    RoleDetails = angular.element("#grid-table").scope();
     Scope.role = {users:[], apps:[]};
     Scope.action = "Create new ";
     $('#update_button').hide();
-    Scope.Roles = RolesRelated.get();
+    Scope.components = [{name:"*", label:"All"}];
+    Scope.component = '*';
     Scope.AllUsers = User.get();
     Scope.Apps = App.get();
+    Scope.service = {service_id:null,access:"Full Access", component: "*"};
+    Scope.selectServices = {};
+    Scope.Services = Service.get(function(data){
+        var services = data.record;
+        services.forEach(function(service){
+            if(service.type.contains("SQL")){
+                $http.get('/rest/'+ service.api_name + '/?app_name=admin&fields=*').success(function(data){
+                    service.components = data.resource;
+                    Scope.selectServices[service.id] = data.resource;
+                });
+            }
+        });
+        if(Scope.Services.record){
+            Scope.Services.record.push({id: null, name: "All"})
+        }
+    });
+    Scope.Roles = RolesRelated.get();
     Scope.save = function () {
         var id = this.role.id;
         RolesRelated.update({id:id}, Scope.role, function () {
@@ -18,31 +35,30 @@ var RoleCtrl = function ($scope, RolesRelated, User, App) {
         });
     };
     Scope.isUserInRole = function(){
-     var inRole = false;
-     if(Scope.role.users.length > 0){
-     if(this.user.role_id == Scope.role.id){
-        inRole = true;
-     }
-     return inRole;
-     }
+        var inRole = false;
+        if(Scope.role.users.length > 0){
+            if(this.user.role_id == Scope.role.id){
+                inRole = true;
+            }
+            return inRole;
+        }
     };
     Scope.isAppInRole = function(){
         var inGroup =false;
         if(Scope.role){
-        var id = this.app.id;
-        var assignedApps = Scope.role.apps;
-        assignedApps = $(assignedApps);
+            var id = this.app.id;
+            var assignedApps = Scope.role.apps;
+            assignedApps = $(assignedApps);
 
-        assignedApps.each(function(index, val){
-            if(val.id == id){
-                inGroup = true;
-            }
-        });
+            assignedApps.each(function(index, val){
+                if(val.id == id){
+                    inGroup = true;
+                }
+            });
         }
         return inGroup;
     };
     Scope.addAppToRole = function (checked) {
-
         if(checked == true){
             Scope.role.apps.push(this.app);
         }else{
@@ -50,14 +66,25 @@ var RoleCtrl = function ($scope, RolesRelated, User, App) {
         }
     };
     $scope.updateUserToRole = function (checked) {
-
         if(checked == true){
-           Scope.role.users.push(this.user);
+            Scope.role.users.push(this.user);
         }else{
-           Scope.role.users = removeByAttr(Scope.role.users, 'id', this.user.id);
+            Scope.role.users = removeByAttr(Scope.role.users, 'id', this.user.id);
         }
-
     };
+    Scope.loadComponents = function(){
+        Scope.components = [];
+        Scope.components.push({ name:"*", label:"All"});
+        Scope.components = Scope.selectServices[Scope.service.service_id];
+    };
+    Scope.loadIndComponents = function(){
+        this.components = [];
+        this.components.push({ name:"*", label:"All"});
+        this.components = Scope.selectServices[this.service_access.service_id];
+    };
+    Scope.addServiceAccess = function(){
+        Scope.role.role_service_accesses.push(Scope.service);
+    }
     removeByAttr = function(arr, attr, value){
         var i = arr.length;
         while(i--){
@@ -83,6 +110,7 @@ var RoleCtrl = function ($scope, RolesRelated, User, App) {
     $scope.showDetails = function () {
         Scope.action = "Edit this ";
         Scope.role = this.role;
+        Scope.service.role_id = this.role.id;
         Scope.users = this.role.users;
         $('#save_button').hide();
         $('#update_button').show();
