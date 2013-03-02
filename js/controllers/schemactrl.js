c43dbddb740a5e03e2765a665a471a51e89aac71
Jason Sykes <jason@jasonsykes.com>
2013-03-01 23:06:41 -0500

    boolean template refactored to use ng-options

--- js/controllers/schemactrl.js
+++ js/controllers/schemactrl.js
@@ -2,8 +2,8 @@ var SchemaCtrl = function ($scope, Schema, DB) {
     $("#grid-container").hide();
     Scope = $scope;
     Scope.tableData = [];
-    Scope.booleanOptions = [{value:'true', text:'true'},{value:'false', text:'false'}];
-    var booleanTemplate = '<select class="ngCellText colt{{$index}}" ng-model="row.entity[col.field]" ng-change="enableSave()"><option value=true>True</option><option value=false>False</option></select>';
+    Scope.booleanOptions = [{value:true, text:'true'},{value:false, text:'false'}];
+    var booleanTemplate = '<select class="ngCellText colt{{$index}}" ng-options="option.value as option.text for option in booleanOptions" ng-model="row.entity[col.field]" ng-change="enableSave()"></select>';
     var inputTemplate = '<input class="ngCellText colt{{$index}}" ng-model="row.entity[col.field]" ng-change="enableSave()" />';
     var customHeaderTemplate = '<div class="ngHeaderCell">Save</div><div ng-style="{\'z-index\': col.zIndex()}" ng-repeat="col in visibleColumns()" class="ngHeaderCell col{{$index}}" ng-header-cell></div>';
     var buttonTemplate = '<div><button id="save_{{row.rowIndex}}" class="btn btn-small btn-inverse" disabled=true ng-click="saveRow()"><li class="icon-save"></li></button></div>';
