9d5ba2b7ee65ef747675b9f5635039d9471199c9
Jason Sykes <jason@jasonsykes.com>
2013-03-05 15:33:47 -0500

    committing context-less file manager

--- js/controllers/filectrl.js
+++ js/controllers/filectrl.js
@@ -1,6 +1,5 @@
 var FileCtrl = function ($scope, $location, $timeout) {
     Scope = $scope;
-
     Scope.uploadPackage = function () {
         document.forms["upload-form"].submit();
         $timeout(function () {
