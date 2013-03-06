af865422b39739923b43c11ff3255e744b6b97f7
Todd <todd@dreamfactory.com>
2013-03-06 17:14:28 -0500

    Import package file from URL.

--- js/controllers/filectrl.js
+++ js/controllers/filectrl.js
@@ -1,12 +1,18 @@
 var FileCtrl = function ($scope, $location, $timeout) {
     Scope = $scope;
-    Scope.uploadPackage = function () {
-        document.forms["upload-form"].submit();
+    Scope.uploadPackageFile = function () {
+        document.forms["upload-file-form"].submit();
         $timeout(function () {
             $location.path('/app');
         }, 1000);
 
     };
+    function uploadPackageUrl() {
+        document.forms["upload-url-form"].submit();
+        $timeout(function () {
+            $location.path('/app');
+        }, 1000);
+    }
 
     $("#root-file-manager").css('height', $(window).height()).css('width', '100%').show();
     $("#root-file-manager iframe").css('height', $(window).height()).css('width', '100%').attr("src", 'http://' + location.host + '/public/admin/filemanager/?path=/').show();
