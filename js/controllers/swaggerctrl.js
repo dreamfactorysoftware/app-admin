83dd66ec1e372c414fcc4b437c546aeb34d9bed7
Jason Sykes <jasonsykes@dreamfactory.com>
2013-03-12 16:35:08 -0400

    Swagger show() moved to navigation

--- js/controllers/swaggerctrl.js
+++ js/controllers/swaggerctrl.js
@@ -1,24 +1,22 @@
 var SwaggerCtrl = function ($rootScope, $timeout) {
 
-    $rootScope.loadSwagger = function(hash){
+    $rootScope.loadSwagger = function (hash) {
         $("#swagger iframe").attr('src', '');
         var appendURL = "";
-        if(hash){
+        if (hash) {
             appendURL = "/#!/" + hash;
         }
         $("#swagger").css('height', $(window).height()).css('width', '100%').show();
 
-        $timeout(function(){
+        $timeout(function () {
             $("#swagger iframe").css('height', $(window).height()).css('width', '100%').attr("src", CurrentServer + '/public/admin/swagger/' + appendURL).show();
 
-        },1000);
+        }, 1000);
 
         $(window).resize(function () {
             $('#swagger').css('height', $(window).height()).css('width', '100%').css('width', '100%');
             $("#swagger iframe").css('height', $(window).height()).css('width', '100%');
         });
     }
-    if(!Scope.action){
-        $rootScope.loadSwagger();
-    }
-}
+    $("#swagger").show();
+};
