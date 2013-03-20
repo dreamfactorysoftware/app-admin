5e38c8cb16a12c1b27f2235e923a69d983acc501
Todd <todd@dreamfactory.com>
2013-03-20 11:43:27 -0400

    Fixed spacing issues and added status container.

--- js/controllers/filectrl.js
+++ js/controllers/filectrl.js
@@ -2,16 +2,9 @@ var FileCtrl = function ($scope, $location, $timeout) {
     Scope = $scope;
     Scope.uploadPackageFile = function () {
         document.forms["upload-file-form"].submit();
-        /*$timeout(function () {
-            $location.path('/app');
-        }, 1000);*/
-
     };
     Scope.uploadPackageUrl = function () {
         document.forms["upload-url-form"].submit();
-        /*$timeout(function () {
-            $location.path('/app');
-        }, 1000);*/
     }
     $("#root-file-manager").css('height', $(window).height()).css('width', '100%').show();
     $("#root-file-manager iframe").css('height', $(window).height()).css('width', '100%').attr("src", CurrentServer + '/public/admin/filemanager/?path=/&allowroot=true').show();
@@ -28,9 +21,9 @@ function checkResults(iframe) {
         if (isErrorString(str)) {
             var response = {};
             response.responseText = str;
-            alertErr(response);
+            window.top.Actions.showStatus(getErrorString(response), "error");
         } else {
-            alert("The app was imported successfully!");
+            window.top.Actions.showStatus("The app was imported successfully!");
         }
     }
 }
