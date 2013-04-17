9b7cf423b57841e8aaf3005180da4ac8e2502c36
Todd <todd@dreamfactory.com>
2013-04-17 09:17:37 -0400

    Checkpoint commit for user invite.

--- js/controllers/userctrl.js
+++ js/controllers/userctrl.js
@@ -3,52 +3,75 @@ var UserCtrl = function ($scope, User, Role) {
     Scope.Users = User.get();
     Scope.action = "Create";
     Scope.Roles = Role.get();
+    Scope.sendInvite = true;
+    Scope.passwordEdit = false;
     Scope.user = {};
-    $('#update_button').hide();
+    Scope.user.password = '';
+    Scope.passwordRepeat = '';
+
     Scope.formChanged = function () {
         $('#save_' + this.user.id).removeClass('disabled');
     };
 
     Scope.save = function () {
-        if(this.user.password == ''){
+        if (this.passwordEdit) {
+            if (this.user.password == '' || this.user.password != this.passwordRepeat) {
+                window.top.Actions.showStatus("Please enter matching passwords", "error");
+                return;
+            }
+        } else {
             delete this.user.password;
         }
         var id = Scope.user.id;
-        User.update({id:id}, Scope.user, function(){
+        User.update({id:id}, Scope.user, function() {
             updateByAttr(Scope.Users.record, 'id', id, Scope.user);
             Scope.promptForNew();
             window.top.Actions.showStatus("Updated Successfully");
+        }, function(response) {
+                Scope.user.password = '';
+                Scope.passwordRepeat = '';
+                window.top.Actions.showStatus(getErrorString(response), "error");
         });
     };
     Scope.create = function () {
         var newRec = this.user;
-        if(this.user.password == ''){
-            delete this.user.password;
+        if (!this.sendInvite) {
+            if (newRec.password == '' || newRec.password != this.passwordRepeat) {
+                window.top.Actions.showStatus("Please enter matching passwords", "error");
+                return;
+            }
+        } else {
+            delete newRec.password;
         }
         if(!newRec.display_name){
             newRec.display_name = newRec.first_name + ' ' + newRec.last_name;
         }
 
-        User.save(newRec, function(data){
-            Scope.promptForNew();
-            Scope.Users.record.push(data);
-            window.top.Actions.showStatus("Created Successfully");
-        });
-
-
+        User.save(newRec,
+            function(response) {
+                Scope.promptForNew();
+                Scope.Users.record.push(response);
+                window.top.Actions.showStatus("Created Successfully");
+            },
+            function(response) {
+                Scope.user.password = '';
+                Scope.passwordRepeat = '';
+                window.top.Actions.showStatus(getErrorString(response), "error");
+            });
+    };
+    Scope.invite = function () {
+        alert("Invite!");
     };
     Scope.promptForNew = function () {
-        $(':checkbox').removeAttr('checked');
         Scope.action = "Create";
+        Scope.sendInvite = true;
+        Scope.passwordEdit = false;
         Scope.user = {};
         Scope.user.password = '';
-        $('#save_button').show();
-        $('#update_button').hide();
-        $('#passwordError').hide();
-        $('#passwordRepeat').val('');
-        Scope.userform.$setPristine();
+        Scope.passwordRepeat = '';
         $("tr.info").removeClass('info');
         $(window).scrollTop(0);
+        Scope.userform.$setPristine();
     };
     Scope.delete = function () {
         var which = this.user.display_name;
@@ -65,19 +88,20 @@ var UserCtrl = function ($scope, User, Role) {
             Scope.promptForNew();
             $("#row_" + id).fadeOut();
             window.top.Actions.showStatus("Deleted Successfully");
+        }, function(response) {
+                window.top.Actions.showStatus(getErrorString(response), "error");
         });
     };
     Scope.showDetails = function(){
         Scope.action = "Edit";
+        Scope.sendInvite = true;
+        Scope.passwordEdit = false;
         Scope.user = angular.copy(this.user);
         Scope.user.password = '';
-        $('#save_button').hide();
-        $('#update_button').show();
-        $('#passwordError').hide();
-        $('#passwordRepeat').val('');
-        Scope.userform.$setPristine();
+        Scope.passwordRepeat = '';
         $("tr.info").removeClass('info');
         $('#row_' + Scope.user.id).addClass('info');
+        Scope.userform.$setPristine();
     }
     Scope.toggleRoleSelect = function (checked) {
 
@@ -87,5 +111,4 @@ var UserCtrl = function ($scope, User, Role) {
             $('#role_select').prop('disabled', false);
         }
     };
-
 };
