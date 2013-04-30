643e229228d3cbf010f2625480ddd55d4aa3093d
Jason Sykes <jasonsykes@dreamfactory.com>
2013-04-30 12:44:46 -0400

    session error handling / pnotify

--- js/controllers/config.js
+++ js/controllers/config.js
@@ -1,12 +1,65 @@
 var ConfigCtrl = function ($scope, Config, Role) {
     Scope = $scope;
-    Scope.Config = Config.get();
-    Scope.Roles = Role.get();
+    Scope.Config = Config.get(function(){}, function(response){
+        var code = response.status;
+        if(code == 401){
+            window.top.Actions.doSignInDialog("stay");
+            return;
+        }
+        var error = response.data.error;
+        $.pnotify({
+            title: 'Error' ,
+            type: 'error',
+            hide:false,
+            addclass: "stack-bottomright",
+            text: error[0].message
+        });
+
+
+
+    });
+    Scope.Roles = Role.get(function(){}, function(response){
+        var code = response.status;
+        if(code == 401){
+            window.top.Actions.doSignInDialog("stay");
+            return;
+        }
+        var error = response.data.error;
+        $.pnotify({
+            title: 'Error' ,
+            type: 'error',
+            hide:false,
+            addclass: "stack-bottomright",
+            text: error[0].message
+        });
+
+
+
+    });
     Scope.save = function () {
         Config.update(Scope.Config, function() {
-            window.top.Actions.showStatus("Updated Successfully");
-        }, function(response) {
-            window.top.Actions.showStatus(getErrorString(response), "error");
-        });
+                $.pnotify({
+                    title: 'Configuration',
+                    type: 'success',
+                    text: 'Updated Successfully'
+                });
+            },
+            function(response){
+                var code = response.status;
+                if(code == 401){
+                    window.top.Actions.doSignInDialog("stay");
+                    return;
+                }
+                var error = response.data.error;
+                $.pnotify({
+                    title: 'Error' ,
+                    type: 'error',
+                    hide:false,
+                    addclass: "stack-bottomright",
+                    text: error[0].message
+                });
+
+
+            });
     };
 }
