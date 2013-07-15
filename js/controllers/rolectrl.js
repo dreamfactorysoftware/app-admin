c7bd5e187faaefdac91230e82cbb869abc2ec0da
Jason Sykes <jasonsykes@dreamfactory.com>
2013-07-15 11:28:22 -0400

    added reset on promptForNew for create RSA form.

--- js/controllers/rolectrl.js
+++ js/controllers/rolectrl.js
@@ -274,6 +274,8 @@ var RoleCtrl = function ($scope, RolesRelated, User, App, Service, $http) {
         angular.element(":checkbox").attr('checked', false);
         Scope.action = "Create new";
         Scope.actioned = "Created";
+        Scope.service = {service_id: null, access: "Full Access", component: "*"};
+        Scope.selectServices = {};
         Scope.role = {users: [], apps: [], role_service_accesses: []};
         $('#save_button').show();
         $('#update_button').hide();
