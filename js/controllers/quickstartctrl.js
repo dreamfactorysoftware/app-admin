b10db3ad479c873bf691849487ad34701aa085ee
Jason Sykes <jasonsykes@dreamfactory.com>
2013-10-11 14:17:39 -0400

    CORS info

--- js/controllers/quickstartctrl.js
+++ js/controllers/quickstartctrl.js
@@ -1,57 +1,126 @@
 var QuickStartCtrl = function ($scope, App, Config, Service) {
     setCurrentApp('getting_started');
-    $scope.Config = Config.get();
-    $scope.Services = Service.get(function(){
+    Scope = $scope;
+    Scope.Config = Config.get();
+    Scope.allVerbs = ["GET","POST", "PUT", "MERGE", "PATCH", "DELETE", "COPY"];
+    Scope.Services = Service.get(function(){
 
-        $scope.Services.record.forEach(function(service){
+        Scope.Services.record.forEach(function(service){
             if (service.type.indexOf("Local File Storage") != -1){
-                $scope.defaultStorageID = service.id;
+                Scope.defaultStorageID = service.id;
             }
 
         });
     });
-    $scope.step = 1;
-    $scope.setStep = function(step){
-        if(step == 2 && $scope.app.native =='1'){
-            $scope.step = 4;
-            $scope.create();
+    Scope.step = 1;
+    Scope.setStep = function(step){
+        if(step == 2 && Scope.app.native =='1'){
+            Scope.step = 4;
+            Scope.create();
             return;
         }
 
-        $scope.step = step;
+        Scope.step = step;
     }
-    $scope.launchApp = function(){
-        window.open(location.protocol + '//' + location.host + '/rest/app/applications/' + $scope.app.api_name+ '/index.html', "df-new");
+    Scope.launchApp = function(){
+        window.open(location.protocol + '//' + location.host + '/rest/app/applications/' + Scope.app.api_name+ '/index.html', "df-new");
     }
-    $scope.downloadSDK = function(){
-        $("#sdk-download").attr('src', location.protocol + '//' + location.host + '/rest/system/app/' + $scope.app.id + '?sdk=true')
+    Scope.downloadSDK = function(){
+        $("#sdk-download").attr('src', location.protocol + '//' + location.host + '/rest/system/app/' + Scope.app.id + '?sdk=true')
     }
-    $scope.create = function () {
+    Scope.addHost = function () {
+        Scope.Config.allowed_hosts.push(Scope.CORS.host);
+        Scope.CORS.host = "";
+    }
+    Scope.removeHost = function () {
+        var index = this.$index;
+        Scope.Config.allowed_hosts.splice(index, 1);
+    }
+    Scope.selectAll = function($event){
 
-        if ($scope.app.native == '1') {
-            $scope.app.storage_service_id = null;
-            $scope.app.storage_container = null;
-            $scope.app.launch_url = "";
-            console.log($scope.app);
+        if($event.target.checked){
+            this.host.verbs = Scope.allVerbs;
+        }else{
+            this.host.verbs = [];
+        }
 
-        }else if($scope.app.storage_service_id == 0){
-            $scope.app.storage_service_id = null;
-            $scope.app.storage_container = null;
+    }
+    Scope.toggleVerb = function () {
+
+        var index = this.$parent.$index;
+        if (Scope.Config.allowed_hosts[index].verbs.indexOf(this.verb) === -1) {
+            Scope.Config.allowed_hosts[index].verbs.push(this.verb);
+        } else {
+            Scope.Config.allowed_hosts[index].verbs.splice(Scope.Config.allowed_hosts[index].verbs.indexOf(this.verb), 1);
+        }
+    };
+    Scope.promptForNew = function(){
+        var newhost = {};
+        newhost.verbs = Scope.allVerbs;
+        newhost.host = "";
+        newhost.is_enabled = true;
+        Scope.Config.allowed_hosts.unshift(newhost);
+    }
+    Scope.showCORS = function(){
+        $("#cors-section").show();
+        var height = $(window).height();
+        $('.well.main').css('height', height + 400);
+    }
+    Scope.saveConfig = function () {
+        Config.update(Scope.Config, function () {
+                $.pnotify({
+                    title: 'Configuration',
+                    type: 'success',
+                    text: 'Updated Successfully'
+                });
+            },
+            function (response) {
+                var code = response.status;
+//                if (code == 401) {
+//                    if(window.top.Actions){
+//
+//                    }
+//                    window.top.Actions.doSignInDialog("stay");
+//                    return;
+//                }
+                var error = response.data.error;
+                $.pnotify({
+                    title: 'Error',
+                    type: 'error',
+                    hide: false,
+                    addclass: "stack-bottomright",
+                    text: error[0].message
+                });
+
+
+            });
+    };
+    Scope.create = function () {
+
+        if (Scope.app.native == '1') {
+            Scope.app.storage_service_id = null;
+            Scope.app.storage_container = null;
+            Scope.app.launch_url = "";
+
+
+        }else if(Scope.app.storage_service_id == 0){
+            Scope.app.storage_service_id = null;
+            Scope.app.storage_container = null;
 
         }else{
-            $scope.app.storage_service_id = $scope.defaultStorageID;
-            $scope.app.storage_container = "applications"
+            Scope.app.storage_service_id = Scope.defaultStorageID;
+            Scope.app.storage_container = "applications"
         }
-        $scope.app.name = $scope.app.api_name;
-        App.save($scope.app, function (data) {
+        Scope.app.name = Scope.app.api_name;
+        App.save(Scope.app, function (data) {
                 //Scope.Apps.record.push(data);
-                $scope.app.id = data.id;
+                Scope.app.id = data.id;
                 //Scope.app = data;
                 if(window.top.Actions){
                     window.top.Actions.updateSession("update");
                 }
                 $.pnotify({
-                    title: $scope.app.api_name,
+                    title: Scope.app.api_name,
                     type: 'success',
                     text: 'Created Successfully'
                 });
