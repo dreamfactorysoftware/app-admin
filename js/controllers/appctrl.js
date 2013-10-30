b63d264f5731d8a2d414bec59246425b20be1588
Jason Sykes <jasonsykes@dreamfactory.com>
2013-10-30 10:17:41 -0400

    this change removes hard coding for /app/ as service api_name

--- js/controllers/appctrl.js
+++ js/controllers/appctrl.js
@@ -304,9 +304,9 @@ var AppCtrl = function ($scope, AppsRelated, Role, $http, Service, $location) {
         if (this.app.storage_service_id) {
             container = this.app.storage_container || null;
             container = container ? this.app.storage_container + "/" : '';
-            $("#file-manager iframe").css('height', $(window).height() - 200).attr("src", CurrentServer + '/public/admin/filemanager/?path=/' + Scope.storageContainers[this.app.storage_service_id].name + '/' + container + this.app.api_name + '/&allowroot=false').show();
+            $("#file-manager iframe").css('height', $(window).height() - 200).attr("src", CurrentServer + '/public/admin/filemanager/?path=/' + Scope.storageContainers[this.app.storage_service_id].name + '/' + container + '/' + this.app.api_name + '/&allowroot=false').show();
         } else {
-            $("#file-manager iframe").css('height', $(window).height() - 200).attr("src", CurrentServer + '/public/admin/filemanager/?path=/app/applications/' + this.app.api_name + '/&allowroot=false').show();
+            $("#file-manager iframe").css('height', $(window).height() - 200).attr("src", CurrentServer + '/public/admin/filemanager/?path=/' + Scope.defaultStorageName  + '/applications/' + this.app.api_name + '/&allowroot=false').show();
         }
 
 
